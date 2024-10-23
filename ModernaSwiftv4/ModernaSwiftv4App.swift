5b86a1d7aaa5450f52766b815e6a5f7d61e67dfa
Bill Mårtensson <bill@magictechnology.se>
2024-10-23 14:25:19 +0200

    Initial Commit

---
+++ ModernaSwiftv4/ModernaSwiftv4App.swift
@@ -0,0 +1,17 @@
+//
+//  ModernaSwiftv4App.swift
+//  ModernaSwiftv4
+//
+//  Created by BillU on 2024-10-23.
+//
+
+import SwiftUI
+
+@main
+struct ModernaSwiftv4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ModernaSwiftv4Tests/ModernaSwiftv4Tests.swift
@@ -0,0 +1,17 @@
+//
+//  ModernaSwiftv4Tests.swift
+//  ModernaSwiftv4Tests
+//
+//  Created by BillU on 2024-10-23.
+//
+
+import Testing
+@testable import ModernaSwiftv4
+
+struct ModernaSwiftv4Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
